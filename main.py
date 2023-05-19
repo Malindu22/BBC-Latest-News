1dcbbce51d39ee1afab8e5039500a5e80ba9843d
Malindu22 <malindu.gimhan18@gmail.com>
2023-05-19 16:44:01 +0530

    changes add [C]

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ data = []
 for x in range(14):
     results = soup.find_all("div", {"data-entityid":"container-top-stories#"+str(x+1)})
     for news in results:
-        spanTag = news.find("span", {"class": "gs-u-vh"})
-        obj = {
-            "id" : x+1,
-            "title" : news.h3.get_text(),
-            "description" : news.p.get_text(),
-            "timeZone" : news.time['datetime'],
-            "updated_at" : spanTag.get_text(),
-            "link" :  'https://www.bbc.com'+ news.a['href']
-        }
-        data.append(obj)
+        timeTag = news.find("time", {"class": "qa-status-date"})
+        if timeTag is None :
+            obj = {
+                "id" : x+1,
+                "title" : news.h3.get_text(),
+                "description" : news.p.get_text(),
+                "timeZone" : "Now",
+                "link" :  'https://www.bbc.com'+ news.a['href']
+            }
+            data.append(obj)
+        else :
+            obj = {
+                "id" : x+1,
+                "title" : news.h3.get_text(),
+                "description" : news.p.get_text(),
+                "timeZone" : timeTag['datetime'],
+                "link" :  'https://www.bbc.com'+ news.a['href']
+            }
+            data.append(obj)
 
 
 print('\033[1;37;43m' + '\033[1m' + "\033[1;34;43m Fetch Successfull ! \033[49m \n Write Json \n Your Data is ready \033[1;31;40m :) \033[49m")
